255b145a878834b871c3804123f72d2252075aba
hucheng <hucheng9110@gmail.com>
2020-08-16 15:44:31 +0800

    文件上传例子

--- koa-file-upload/controller/user.js
+++ koa-file-upload/controller/user.js
@@ -4,7 +4,7 @@
  * @Date: 2020-06-22 07:21:50
  * @Description: here is des
  */ 
-const array = [{id: 0, name: '王二', email: 'wanger@gmail.com'}, {id: 1, name: '赵三', email: 'zhaosan@gmail.com'}]
+const array = [{id: 0, name: '王二', email: 'wanger@gmail.com'}, {id: 1, name: '赵三', email: 'wanger@gmail.com'}]
 
 function list(ctx) {
     const body = {
@@ -21,12 +21,29 @@ function detail(ctx) {
     }
 }
 function add(ctx) {
-    const { path } =  ctx.request.files.file
+    // 获取文件路径，这里注意，我在前端 发起请求的时候，FormData 的 key 是 file，所以这里用 *.file，可以接着看多文件上传 
+        const { path } =  ctx.request.files.file 
+        const { name, email } = ctx.request.body // 获取 request body 字段
+        console.log(path)
+        array.push({
+            id: array.length,
+            imgUrl: path.split("/static")[1],
+            name,
+            email
+        })
+        ctx.body = {
+            uccess: true
+        }
+}
+function add(ctx) {
+    const { file1, file2 } =  ctx.request.files // 这里注意，我在前端 发起请求的时候，FormData 的 key 是 file，所以这里用 *.file 
+    const path1 = file1.path
+    const path2 = file2.path
     const { name, email } = ctx.request.body
-    console.log(path)
+    console.log(path1, path2)
     array.push({
         id: array.length,
-        imgUrl: path.split("/static")[1],
+        imgUrl: path1.split("/static")[1],
         name,
         email
     })
@@ -34,7 +51,6 @@ function add(ctx) {
         success: true
     }
 }
-
 module.exports = {
     detail,
     list,
