6aadc0da71a146ad0fc889e5ad5f97af62cf1d01
hucheng <hucheng9110@gmail.com>
2020-08-23 20:19:38 +0800

    feat: update

--- koa-mongo/app/db/mogodb.js
+++ koa-mongo/app/db/mogodb.js
@@ -3,6 +3,9 @@ const { mongodb } = require('../config')
 const { logger } = require('../log4j/logger')
 const { port, host, pass, userName, database, authSource = 'admin', poolSize = 40 } = mongodb
 let connection
+/**
+ * 这里我们初始化数据库连接
+ */
 async function  initDB() {
     let mongoUrl
     if (userName && pass) {
@@ -11,9 +14,7 @@ async function  initDB() {
         mongoUrl = `mongodb://${host}:${port}`
     }
     return new Promise((resolve, reject) => {
-        MongoClient.connect(mongoUrl, {poolSize: poolSize} ,function(err, client) {
-
-            
+        MongoClient.connect(mongoUrl, {poolSize: poolSize} ,function(err, client) {          
             if(err) {
                 logger.error(err)
                 reject(err)
